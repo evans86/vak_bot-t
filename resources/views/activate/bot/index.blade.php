b01c3d77e157fd246fef492e1fa789627ab3d435
evans86 <evans86@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>
2023-05-04 01:07:38 +0300

    update resource link

--- resources/views/activate/bot/index.blade.php
+++ resources/views/activate/bot/index.blade.php
@@ -8,7 +8,7 @@
                 <th scope="col">Ключи</th>
                 <th scope="col">Bot-t ID</th>
                 <th scope="col">Версия</th>
-                <th scope="col">API ключ</th>
+                <th scope="col">Links</th>
                 <th scope="col">ID категории</th>
                 <th scope="col">Процент</th>
                 <th scope="col">Баланс</th>
@@ -23,7 +23,7 @@
                     <td>Private: {{ $bot->public_key }}<br>Public: {{ $bot->private_key }}</td>
                     <td>{{ $bot->bot_id }}</td>
                     <td>{{ $bot->version }}</td>
-                    <td>{{ $bot->api_key }}</td>
+                    <td>API key: {{ $bot->api_key }}<br>Link: {{ $bot->resource_link }}</td>
                     <td>{{ $bot->category_id }}</td>
                     <td>{{ $bot->percent }} %</td>
                     <td>{{ \App\Helpers\BotHelpers::balance($bot) }} руб.</td>
