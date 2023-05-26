819b93bc5a0d7fcd8644578bd0b0bd737135d815
evans86 <evans86@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>
2023-05-27 01:05:21 +0300

    update createRentOrder

--- app/Http/Controllers/Api/v1/RentController.php
+++ app/Http/Controllers/Api/v1/RentController.php
@@ -26,6 +26,7 @@ class RentController extends Controller
     public function __construct()
     {
         $this->rentService = new RentService();
+        $this->middleware('admin');
     }
 
     /**
