7bcb420833c05843bfd4dce576eba0e857fc1742
evans86 <evans86@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>
2023-07-14 23:47:00 +0300

    add bot info

--- app/Services/Activate/OrderService.php
+++ app/Services/Activate/OrderService.php
@@ -381,7 +381,7 @@ class OrderService extends MainService
             $this->notifyTelegram($finish_text);
 
         } catch (\Exception $e) {
-            $this->notifyTelegram($e->getMessage());
+            $this->notifyTelegram('🔴' . $e->getMessage());
         }
     }
 
