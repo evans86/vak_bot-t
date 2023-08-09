a1cb97c0514eba952f7bee627385c2973576f3fa
evans86 <evans86@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>
2023-08-09 19:45:46 +0300

    update create multi

--- app/Http/Controllers/Activate/BotController.php
+++ app/Http/Controllers/Activate/BotController.php
@@ -14,7 +14,7 @@ class BotController
         $bots = SmsBot::orderBy('id', 'DESC')->Paginate(10);
 
         //даты последней выплаты минус бот созданный для примера
-        $newBots = count(SmsBot::query()->where('created_at', '>', '2023-07-10 21:13:25')->get());
+        $newBots = count(SmsBot::query()->where('created_at', '>', '2023-08-09 18:13:25')->get());
         $allCount = count(SmsBot::get());
 
         return view('activate.bot.index', compact(
