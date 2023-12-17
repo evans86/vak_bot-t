f48ae72e540460e9a658fb7e1367038af6e106b8
evans86 <evans86@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>
2023-12-17 12:53:51 +0300

    add cache service & countries

--- app/Http/Controllers/Activate/OrderController.php
+++ app/Http/Controllers/Activate/OrderController.php
@@ -12,7 +12,7 @@ class OrderController
      */
     public function index()
     {
-        $orders = SmsOrder::latest()->take(1000)->Paginate(15);
+        $orders = SmsOrder::latest()->take(100)->Paginate(15);
 
         $allCount = count(SmsOrder::get());
         $successCount = count(SmsOrder::query()->where('status', SmsOrder::STATUS_FINISH)->get());
